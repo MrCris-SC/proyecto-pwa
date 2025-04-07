757b000414f7d445450e72d16ca15fbfbafdbf34
MrCris-SC <cristino_1401@hotmail.com>
2025-04-06 23:36:54 -0600

    Update Concursos.vue

--- resources/js/Pages/ConcursosLayouts/Concursos.vue
+++ resources/js/Pages/ConcursosLayouts/Concursos.vue
@@ -37,18 +37,15 @@ const obtenerConcursoEnPantalla = () => {
 };
 
 const handleMenuSelected = (menu) => {
-    selectedMenu.value = menu; // Actualiza el menú seleccionado
-    showForm.value = selectedMenu.value !== 'concursos'; // Controla si se muestra un formulario
-    
-    if (selectedMenu.value === 'Concursos') {
-      concursoSeleccionado.value = null;
-      obtenerConcursoEnPantalla();
-    } else if (selectedMenu.value === 'gestión de proyectos') {
-        router.get(route('gestion.proyectos')); // Redirige a gestión de proyectos
-    } else if (selectedMenu.value === 'registro de criterios') {
-        // Cambia el componente dinámico sin recargar la página
-        showForm.value = true;
-    }
+  selectedMenu.value = menu; // Usamos el valor exacto recibido
+  showForm.value = menu !== 'Concursos'; // Comparación exacta
+  
+  if (menu === 'Gestión de proyectos') {
+    router.get(route('gestion.proyectos'));
+  } else if (menu === 'Concursos') {
+    concursoSeleccionado.value = null;
+    obtenerConcursoEnPantalla();
+  }
 };
 
 const handleDownloadPDF = () => {
